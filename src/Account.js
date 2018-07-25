31027bdf3edab36d59bb478347ce0b7339811c8d
Alex Werner <alex@werner.fr>
2018-07-25 22:47:17 +0200

    Doc : Update documentation

--- src/Account.js
+++ src/Account.js
@@ -352,53 +352,6 @@ class Account {
 
     return privKeys;
   }
-
-  /*
- * Evo L1 stuff
- */
-
-  // /**
-  //  * @param {string} username
-  //  * @returns {string} - hex string containing user registration
-  //  */
-  // createRegistration(username) {
-  //   const privateKey = new PrivateKey(this.keychain.getNewPrivateKey());
-  //   return Registration.createRegistration(username, privateKey).serialize();
-  // }
-  //
-  // /**
-  //  * @param {string} rawRegistration - hex string representing user registration data
-  //  * @param {number} [funding] - default funding for the account in duffs. Optional.
-  // If left empty,
-  //  * funding will be 0.
-  //  * @return {string} - user id
-  //  */
-  // async registerUser(rawRegistration, funding = 0) {
-  //   const regTx = new Registration(rawRegistration);
-  //   const UTXO = await this.getUTXO();
-  //   regTx.fund(UTXO, this.getNewAddress(), funding);
-  //   const signedTx = this.signTransaction(regTx.serialize());
-  //   return this.sendTransaction(signedTx);
-  // }
-  //
-  // /**
-  //  * @param {string} userId
-  //  * @param {number} amount - top up amount in duffs
-  //  * @return {Promise<string>} - tx id
-  //  */
-  // async topUpUserCredits(userId, amount) {
-  //   const inputs = await this.getUTXO();
-  //   const subTx = new TopUp();
-  //   subTx.fund(userId, amount, inputs, this.getNewAddress());
-  //   const signedTx = this.signTransaction(subTx.serialize());
-  //   return this.sendTransaction(signedTx);
-  // }
-  //
-  // async signStateTransitionHeader(rawHeader) {
-  //   const ts = new Transaction(rawHeader);
-  //   ts.sign(this.getPrivateKeyForSigning());
-  //   return ts.serialize();
-  // }
 }
 
 module.exports = Account;
