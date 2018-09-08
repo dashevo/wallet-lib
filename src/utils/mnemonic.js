38c93996ce29f7cb673bb70dfcb83b1283160d34
Alex Werner <alex@werner.fr>
2018-09-08 12:06:25 +0200

    Feat : Added WalletId utils

--- src/utils/mnemonic.js
+++ src/utils/mnemonic.js
@@ -1,4 +1,5 @@
 const Mnemonic = require('@dashevo/dashcore-mnemonic');
+const { hash256 } = require('./crypto');
 
 function generateNewMnemonic() {
   return Mnemonic();
@@ -20,15 +21,30 @@ function generateNewMnemonic() {
  */
 function mnemonicToSeed(mnemonic, network, passphrase = '') {
   if (!mnemonic) throw new Error('Expect mnemonic to be provided');
-
+  // const seed = (mnemonic.constructor.name === 'Mnemonic')
+  //   ? mnemonic.toHDPrivateKey(passphrase, network)
+  //   : new Mnemonic(mnemonic).toHDPrivateKey(passphrase, network);
+  // if (mnemonic.constructor.name === 'Mnemonic') {
+  //   console.log('--',new Mnemonic(mnemonic).toHDPrivateKey(passphrase, network));
+  // }
+  // console.log('mnemonictoseed');
+  // return seed;
   return (mnemonic.constructor.name === 'Mnemonic')
     ? mnemonic.toHDPrivateKey(passphrase, network)
     : new Mnemonic(mnemonic).toHDPrivateKey(passphrase, network);
 }
 
+function mnemonicToWalletId(mnemonic) {
+  if (!mnemonic) throw new Error('Expect mnemonic to be provided');
+  const buffMnemonic = Buffer.from(mnemonic.toString())
+  const buff = hash256(buffMnemonic);
+  const walletId = buff.toString('hex').slice(0, 10);
+  return walletId;
+}
 
 module.exports = {
   generateNewMnemonic,
   // HDPrivateKeyToMnemonic,
   mnemonicToSeed,
+  mnemonicToWalletId
 };
--- test/utils/mnemonic.js
+++ test/utils/mnemonic.js
@@ -1,5 +1,10 @@
 const { expect } = require('chai');
-const {generateNewMnemonic, mnemonicToSeed} = require('../../src/utils/mnemonic');
+const {generateNewMnemonic, mnemonicToSeed, mnemonicToWalletId} = require('../../src/utils/mnemonic');
+const is = require('../../src/utils/is');
+
+const mnemonic1 = 'hole lesson insane entire dolphin scissors game dwarf polar ethics drip math';
+const mnemonic2 = 'woman forest output essay bleak satisfy era ordinary exotic source portion wire';
+const mnemonic3 = 'divorce radar castle wire sun timber master income exchange wash fluid loud'
 
 describe('Utils - mnemonic', () => {
   it('should generate new mnemonic', () => {
@@ -13,5 +18,16 @@ describe('Utils - mnemonic', () => {
     const result2 = mnemonicToSeed(mnemonic2);
     expect(result.constructor.name).to.be.equal('HDPrivateKey');
     expect(result2.constructor.name).to.be.equal('HDPrivateKey');
+  });
+  it('should do mnemonicToWalletId', () => {
+    const mnemonic = generateNewMnemonic();
+    const result = mnemonicToWalletId(mnemonic);
+    expect(result.constructor.name).to.be.equal('String');
+    expect(result.length).to.be.equal(10);
+    expect(is.hex(result)).to.be.equal(true);
+
+    expect(mnemonicToWalletId(mnemonic1)).to.equal('f566600d81');
+    expect(mnemonicToWalletId(mnemonic2)).to.equal('74bbe91a47');
+    expect(mnemonicToWalletId(mnemonic3)).to.equal('f351a836e6');
   });
 });
