7fbaf561695f6668c89bab9de1287d921a598a13
Alex Werner <alex@werner.fr>
2018-10-13 06:55:21 +0200

    Impr : Added HDPrivateKey validator

--- src/utils/is.js
+++ src/utils/is.js
@@ -1,5 +1,5 @@
 //Todo : Some validators here are really proto type of methods, urgent impr is needed here.
-const { PrivateKey } = require('@dashevo/dashcore-lib');
+const { PrivateKey,HDPrivateKey } = require('@dashevo/dashcore-lib');
 const is = {
   // Primitives
   arr: arr => Array.isArray(arr) || arr.constructor.name === 'Array',
@@ -21,7 +21,7 @@ const is = {
   mnemonic: mnemonic => is.string(mnemonic) || is.type(mnemonic, 'Mnemonic'),
   network: network => is.string(network) || is.type(network, 'Network'),
   privateKey: pKey => is.type(pKey, 'PrivateKey') || (is.string(pKey) && PrivateKey.isValid(pKey)),
-  seed: seed => is.string(seed) || is.type(seed, 'HDPrivateKey'),
+  seed: seed => is.type(seed, 'HDPrivateKey') || (is.string(seed) && HDPrivateKey.isValidSerialized(seed)),
   address: addr => is.string(addr) || is.type(addr, 'Address'),
   transaction: tx => is.obj(tx) && tx.vin && is.arr(tx.vin) && tx.vout && is.arr(tx.vout),
   feeRate: feeRate => is.obj(feeRate) && is.string(feeRate.type) && is.int(feeRate.value),
