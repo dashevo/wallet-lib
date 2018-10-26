cfb16d11873afbf5815e169962e3ca177d683678
Alex Werner <alex@werner.fr>
2018-10-26 01:04:14 +0100

    Doc : Wallet DAPI example

--- examples/wallet-dapi.js
+++ examples/wallet-dapi.js
@@ -2,25 +2,28 @@ const DAPIClient = require('@dashevo/dapi-client');
 const Wallet = require('../src/Wallet');
 
 const start = async () => {
-  const transport = new DAPIClient({ seeds: [{ ip: '18.237.151.230', port: 3000 }] });
+  const transport = new DAPIClient({ seeds: [{ ip: '54.255.164.83', port: 3000 }] });
+  transport.getTransaction = transport.getTransactionById;
+
   const mnList = await transport.MNDiscovery.getMNList();
   const peersIP = mnList.map(el => ({ ip } = el.ip));
+
+  console.log('List of known peer', peersIP);
 
   const wallet = new Wallet({
     network: 'testnet',
     transport,
     mnemonic: 'protect cave garden achieve hand vacant clarify atom finish outer waste sword',
   });
-  console.log('New Clients :', peersIP);
 
-  console.log(wallet.exportWallet());
+  console.log("Random generated mnemonic:",wallet.exportWallet());
+
   const account = wallet.getAccount(0);
-  console.log(account);
 
   const balance = account.getBalance();
-  console.log(balance);
+  console.log("Balance",balance);
 
-  const address = account.getUnusedAddress();
-  console.log(address);
+  const { address } = account.getUnusedAddress();
+  console.log("New unused address", address);
 };
 start();
