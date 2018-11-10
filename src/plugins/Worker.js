efcf20bb0160231dff7a3bcdc149cbe929373670
Alex Werner <alex@werner.fr>
2018-11-10 08:10:52 +0100

    Feat : Added workerMaxPass option

--- src/plugins/Worker.js
+++ src/plugins/Worker.js
@@ -5,6 +5,7 @@ const defaultOpts = {
   workerIntervalTime: 10 * 1000,
   executeOnStart: false,
   firstExecutionRequired: false,
+  workerMaxPass: null,
 };
 
 class Worker extends StandardPlugin {
@@ -25,6 +26,10 @@ class Worker extends StandardPlugin {
     this.workerIntervalTime = (opts.workerIntervalTime)
       ? opts.workerIntervalTime
       : defaultOpts.workerIntervalTime;
+
+    this.workerMaxPass = (opts.workerMaxPass)
+      ? opts.workerMaxPass
+      : defaultOpts.workerMaxPass;
   }
 
   async startWorker() {
@@ -32,14 +37,10 @@ class Worker extends StandardPlugin {
     if (this.worker) this.stopWorker();
     // every minutes, check the pool
     this.worker = setInterval(this.execWorker.bind(self), this.workerIntervalTime);
-    if (this.execDuringStart) {
-      await this.execDuringStart();
-    }
     if (this.executeOnStart === true) {
       await this.execWorker();
     }
     this.events.emit(`WORKER/${this.constructor.name.toUpperCase()}/STARTED`);
-
   }
 
   stopWorker() {
@@ -54,6 +55,11 @@ class Worker extends StandardPlugin {
     if (this.isWorkerRunning) {
       return false;
     }
+    if (this.workerMaxPass!==null && this.workerPass >= this.workerMaxPass) {
+      console.log('Max pass, shutting down');
+      this.stopWorker();
+      return false;
+    }
     this.isWorkerRunning = true;
 
     if (this.execute) {
@@ -62,7 +68,7 @@ class Worker extends StandardPlugin {
       console.error(`${this.constructor.name} : Missing execute function`);
     }
 
-    this.workerRunning = false;
+    this.isWorkerRunning = false;
     this.workerPass += 1;
 
     this.events.emit(`WORKER/${this.constructor.name.toUpperCase()}/EXECUTED`);
