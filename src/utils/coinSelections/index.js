fb0f51e78dd9dac037bf8ae2b423f15a570f6afc
Joshua Seigler <seigler@users.noreply.github.com>
2018-11-15 10:24:35 -0500

    Improving error message clarity

--- src/utils/coinSelections/index.js
+++ src/utils/coinSelections/index.js
@@ -3,9 +3,9 @@ const STRATEGIES = require('./strategies');
 const { InvalidUTXO, InvalidOutput, CoinSelectionUnsufficientUTXOS } = require('../../errors');
 
 module.exports = function coinSelection(utxosList, outputsList, deductFee = false, feeCategory = 'normal', strategyName = 'simpleAccumulator') {
-  if (!utxosList) { throw new Error('Require a utxosList to select from'); }
-  if (utxosList.constructor.name !== 'Array') { throw new Error('Require utxosList to be an array of utxos'); }
-  if (utxosList.length < 1) { throw new Error('Require utxosList to contains at least 1 utxo'); }
+  if (!utxosList) { throw new Error('A txosList is required'); }
+  if (utxosList.constructor.name !== 'Array') { throw new Error('utxosList must be an array of utxos'); }
+  if (utxosList.length < 1) { throw new Error('utxosList must contain at least 1 utxo'); }
   let utxosValue = 0;
   utxosList.forEach((utxo) => {
     if (!is.utxo(utxo)) {
@@ -15,9 +15,9 @@ module.exports = function coinSelection(utxosList, outputsList, deductFee = fals
   });
 
 
-  if (!outputsList) { throw new Error('Require a outputsList to perform a selection for'); }
-  if (outputsList.constructor.name !== 'Array') { throw new Error('Require outputsList to be an array of outputs'); }
-  if (outputsList.length < 1) { throw new Error('Require outputsList to contains at least 1 output'); }
+  if (!outputsList) { throw new Error('An outputsList is required in order to perform a selection'); }
+  if (outputsList.constructor.name !== 'Array') { throw new Error('outputsList must be an array of outputs'); }
+  if (outputsList.length < 1) { throw new Error('outputsList must contains at least 1 output'); }
 
   let outputValue = 0;
   outputsList.forEach((output) => {
