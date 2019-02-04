2570cf5abe746a59a6440b6c960acf48bac947b6
Alex Werner <alex@werner.fr>
2019-02-04 22:06:50 +0100

    Fix : Sorting

--- src/utils/coinSelections/helpers/sortAndVerifyUTXOS.js
+++ src/utils/coinSelections/helpers/sortAndVerifyUTXOS.js
@@ -1,2 +1,26 @@
-const sortAndVerifyUTXOS = utxosList => utxosList.sort((a, b) => b.satoshis - a.satoshis);
+const _ = require('lodash');
+
+const sort = {
+  by(el, params) {
+    if (!params) return el;
+
+    const key = params.property;
+    const direction = (params.direction === 'ascending') ? 1 : -1;
+
+    el.sort((a, b) => {
+      let result;
+      if ((a[key] < b[key])) {
+        result = -1;
+      } else {
+        result = (a[key] > b[key]) ? 1 : 0;
+      }
+      return result * direction;
+    });
+
+    return el;
+  },
+};
+
+const sortAndVerifyUTXOS = (utxosList, opts) => sort.by(utxosList, opts);
+
 module.exports = sortAndVerifyUTXOS;
