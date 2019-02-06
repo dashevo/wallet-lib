c52c856cfeef816909d0ae08ee14eef15649a66e
Alex Werner <alex@werner.fr>
2019-02-06 23:42:07 +0100

    Impr : Allow to sort accross multiple properties

--- src/utils/coinSelections/helpers/sortAndVerifyUTXOS.js
+++ src/utils/coinSelections/helpers/sortAndVerifyUTXOS.js
@@ -1,19 +1,23 @@
+/* eslint-disable no-param-reassign */
 const sort = {
   by(el, params) {
     if (!params) return el;
 
-    const key = params.property;
-    const direction = (params.direction === 'ascending') ? 1 : -1;
-
     el.sort((a, b) => {
       let result;
-      if ((a[key] < b[key])) {
-        result = -1;
-      } else {
-        result = (a[key] > b[key]) ? 1 : 0;
-      }
-      return result * direction;
+      params.reverse().forEach((param) => {
+        const key = param.property;
+        const { direction } = (param.direction === 'ascending') ? 1 : -1;
+
+        if ((a[key] < b[key])) {
+          result = -1;
+        } else {
+          result = (a[key] > b[key]) ? 1 : 0;
+        }
+        return result * direction;
+      });
     });
+
 
     return el;
   },
