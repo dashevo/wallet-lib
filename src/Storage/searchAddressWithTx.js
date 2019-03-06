a86807f81c37e95c492dd1bbc40543b6df9b5b06
Alex Werner <alex@werner.fr>
2019-03-06 06:08:23 +0100

    Fix : Additional check on searchAddr

--- src/Storage/searchAddressWithTx.js
+++ src/Storage/searchAddressWithTx.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 /**
  * Search an address having a specific txid
  * todo : Handle when multiples one (inbound/outbound)
@@ -16,20 +17,22 @@ const searchAddressWithTx = function (txid) {
   // Look up by looping over all addresses todo:optimisation
   const existingWallets = Object.keys(store);
   existingWallets.forEach((walletId) => {
-    const existingTypes = Object.keys(store.wallets[walletId].addresses);
-    existingTypes.forEach((type) => {
-      const existingPaths = Object.keys(store.wallets[walletId].addresses[type]);
-      existingPaths.forEach((path) => {
-        const el = store.wallets[walletId].addresses[type][path];
-        if (el.transactions.includes(search.txid)) {
-          search.path = path;
-          search.address = el.address;
-          search.type = type;
-          search.found = true;
-          search.result = el;
-        }
+    if (_.has(store.wallets[walletId], 'addresses')) {
+      const existingTypes = Object.keys(store.wallets[walletId].addresses);
+      existingTypes.forEach((type) => {
+        const existingPaths = Object.keys(store.wallets[walletId].addresses[type]);
+        existingPaths.forEach((path) => {
+          const el = store.wallets[walletId].addresses[type][path];
+          if (el.transactions.includes(search.txid)) {
+            search.path = path;
+            search.address = el.address;
+            search.type = type;
+            search.found = true;
+            search.result = el;
+          }
+        });
       });
-    });
+    }
   });
 
   return search;
