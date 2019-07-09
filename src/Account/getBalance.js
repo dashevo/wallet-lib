7f87daad4bf1d3c6d2bd72562b3f5ca245243f6b
Alex Werner <alex@werner.fr>
2019-07-10 00:42:19 +0200

    test(getBalance): bring getBalance test

--- src/Account/getBalance.js
+++ src/Account/getBalance.js
@@ -1,12 +1,12 @@
 const { duffsToDash } = require('../utils');
 const { WALLET_TYPES } = require('../CONSTANTS');
+
 /**
  * Return the total balance of an account.
  * Expect parralel fetching/discovery to be activated.
  * @return {number} Balance in dash
  */
 function getBalance(unconfirmed = true, displayDuffs = true) {
-  const self = this;
   let totalSat = 0;
   const {
     walletId, storage, accountIndex, type,
@@ -14,13 +14,13 @@ function getBalance(unconfirmed = true, displayDuffs = true) {
   const { addresses } = storage.getStore().wallets[walletId];
   const subwallets = Object.keys(addresses);
   subwallets.forEach((subwallet) => {
-    const paths = Object.keys(self.store.wallets[walletId].addresses[subwallet])
+    const paths = Object.keys(addresses[subwallet])
     // We filter out other potential account
       .filter(el => type === WALLET_TYPES.SINGLE_ADDRESS
-        || parseInt(el.split('/')[3], 10) === accountIndex);
+            || parseInt(el.split('/')[3], 10) === accountIndex);
 
     paths.forEach((path) => {
-      const address = self.store.wallets[walletId].addresses[subwallet][path];
+      const address = addresses[subwallet][path];
       const { unconfirmedBalanceSat, balanceSat } = address;
       totalSat += (unconfirmed) ? unconfirmedBalanceSat + balanceSat : balanceSat;
     });
@@ -28,4 +28,5 @@ function getBalance(unconfirmed = true, displayDuffs = true) {
 
   return (displayDuffs) ? totalSat : duffsToDash(totalSat);
 }
+
 module.exports = getBalance;
--- test/Account/getBalance.js
+++ test/Account/getBalance.js
@@ -1,6 +1,38 @@
 const { expect } = require('chai');
 const Dashcore = require('@dashevo/dashcore-lib');
+const mockedStore = require('../fixtures/sirentonight-fullstore-snapshot-1562711703');
+const getBalance = require('../../src/Account/getBalance');
 
 
-describe('Account - getAddresses', () => {
+let wallet;
+let mockedWallet;
+describe('Account - getBalance', () => {
+  before(() => {
+    const storageHDW = {
+      store: mockedStore,
+      getStore: () => mockedStore,
+      mappedAddress: {},
+    };
+    const walletId = Object.keys(mockedStore.wallets)[0];
+    mockedWallet = Object.assign({
+      walletId,
+      accountIndex: 0,
+      storage: storageHDW,
+    });
+  });
+  it('should correctly get the balance', async () => {
+    const balance = await getBalance.call(mockedWallet);
+    expect(balance).to.equal(184499999506);
+  });
+  it('should correctly get the balance confirmed only', async () => {
+    const balance = await getBalance.call(mockedWallet, false);
+    expect(balance).to.equal(184499999506);
+  });
+  it('should correctly get the balance dash value instead of duff', async () => {
+    const balanceUnconfDash = await getBalance.call(mockedWallet, true, false);
+    const balanceConfDash = await getBalance.call(mockedWallet, false, false);
+
+    expect(balanceUnconfDash).to.equal(1844.99999506);
+    expect(balanceConfDash).to.equal(1844.99999506);
+  });
 });
