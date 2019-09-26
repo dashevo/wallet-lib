3078cf0bf0e49629e52cc38c7365a2daa7f7a7c2
Alex Werner <alex@werner.fr>
2019-09-26 19:32:51 +0200

    docs(Wallet.d): added property definitions

--- src/types/Wallet/methods/Wallet.d.ts
+++ src/types/Wallet/methods/Wallet.d.ts
@@ -2,8 +2,12 @@ import {Mnemonic,PrivateKey, HDPublicKey, Strategy, Network, Plugins} from "../.
 import {Account} from "../../Account/Account";
 
 export declare class Wallet {
+    offlineMode: boolean;
+    allowSensitiveOperations: boolean;
+    injectDefaultPlugins: boolean;
+    plugins:[Plugins];
+    passphrase?:string;
     constructor(options?: Wallet.Options);
-
     createAccount(accOptions: Account.Options): Account;
     disconnect(): void;
     exportWallet():Mnemonic["toString"];
@@ -16,15 +20,17 @@ export declare class Wallet {
     updateNetwork(network:Network):boolean;
 
 }
+
 export declare namespace Wallet {
+
     interface Options {
         offlineMode?: boolean;
         network?: Network;
         plugins?: [Plugins];
         passphrase?: string;
         injectDefaultPlugins?: boolean;
-        mnemonic?: Mnemonic
-        seed?: Mnemonic
+        mnemonic?: Mnemonic|string;
+        seed?: Mnemonic|string;
     }
     interface getAccOptions extends Account.Options{
         index?:number;
