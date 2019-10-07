7e6f973bca082c1446e496fd348c3870ea934e48
Alex Werner <Alex-Werner@users.noreply.github.com>
2019-10-07 12:25:08 +0200

    doc(getIdentityPrivateKey): update JSDoc + name

--- src/types/Account/methods/getIdentityPrivateKey.js
+++ src/types/Account/methods/getIdentityPrivateKey.js
@@ -1,13 +1,13 @@
 /**
- * Return all the private keys matching the PubKey Addr List
+ * Return the identity private key corresponding to the index
  * @param index<number>
  * @return PrivateKey
  */
-function getPrivateKeys(index) {
+function getIdentityPrivateKey(index) {
   // TODO: add proper feature-based derivation
   const { address } = this.getAddress(index);
   const [privateKey] = this.getPrivateKeys([address]);
   return privateKey.privateKey;
 }
 
-module.exports = getPrivateKeys;
+module.exports = getIdentityPrivateKey;
