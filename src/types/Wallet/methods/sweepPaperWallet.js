f1405cd8e1efabdedbd16b9ffd315ac70c343a11
Den <dlilienblum@gmail.com>
2019-10-25 17:37:13 +0400

    sweepPaperWallet refactor

--- src/types/Wallet/methods/sweepPaperWallet.js
+++ src/types/Wallet/methods/sweepPaperWallet.js
@@ -1,10 +1,24 @@
-module.exports = function sweepPaperWallet(privateKey) {
-  const account = this.fromPrivateKey(privateKey).getAccount(0);
-  const amount = account.getTotalBalance();
-  const recipient = this.getAccount(0).getUnusedAddress().address;
-  const tx = account.createTransaction({
-    amount, recipient,
-  });
+const logger = require('../../../logger');
 
-  return account.broadcastTransaction(tx);
+module.exports = function sweepPaperWallet(privateKey) {
+  return new Promise((resolve, reject) => {
+    try {
+      const paperWallet = new this.constructor({ privateKey });
+      const account = paperWallet.getAccount(0);
+      account.events.on('ready', async () => {
+        const amount = account.getTotalBalance();
+        logger.info('Paper wallet balance', amount);
+        const recipient = this.getAccount(0).getUnusedAddress().address;
+        const tx = account.createTransaction({
+          amount, recipient,
+        });
+
+        const result = await account.broadcastTransaction(tx);
+        resolve(result);
+      });
+    } catch (err) {
+      logger.error('sweepPaperWallet', err);
+      reject(err);
+    }
+  });
 };
