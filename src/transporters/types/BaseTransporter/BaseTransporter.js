32a13875dafb8d5e350bef75d70bcf53606cbbec
Alex Werner <alex@werner.fr>
2020-02-27 15:25:13 +0100

    impr(BaseTransporter): added Not Implemented template file

--- src/transporters/types/BaseTransporter/BaseTransporter.js
+++ src/transporters/types/BaseTransporter/BaseTransporter.js
@@ -19,8 +19,18 @@ class BaseTransporter extends EventEmitter {
 BaseTransporter.prototype.announce = require('./methods/announce');
 BaseTransporter.prototype.disconnect = require('./methods/disconnect');
 BaseTransporter.prototype.getAddressSummary = require('./methods/getAddressSummary');
+BaseTransporter.prototype.getBestBlockHash = require('./methods/getBestBlockHash');
+BaseTransporter.prototype.getBestBlockHeight = require('./methods/getBestBlockHeight');
+BaseTransporter.prototype.getBlockByHash = require('./methods/getBlockByHash');
+BaseTransporter.prototype.getBlockByHeight = require('./methods/getBlockByHeight');
+BaseTransporter.prototype.getBlockHeaderByHash = require('./methods/getBlockHeaderByHash');
+BaseTransporter.prototype.getBlockHeaderByHeight = require('./methods/getBlockHeaderByHeight');
+BaseTransporter.prototype.getStatus = require('./methods/getStatus');
 BaseTransporter.prototype.getTransaction = require('./methods/getTransaction');
 BaseTransporter.prototype.getUTXO = require('./methods/getUTXO');
 BaseTransporter.prototype.sendTransaction = require('./methods/sendTransaction');
+BaseTransporter.prototype.subscribeToAddressesTransactions = require('./methods/subscribeToAddressesTransactions');
+BaseTransporter.prototype.subscribeToBlockHeaders = require('./methods/subscribeToBlockHeaders');
+BaseTransporter.prototype.subscribeToBlocks = require('./methods/subscribeToBlocks');
 
 module.exports = BaseTransporter;
