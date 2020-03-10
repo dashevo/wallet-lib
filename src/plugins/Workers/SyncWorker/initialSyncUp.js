5aa610b611f019ef9217f278d5d16c5499e54bfd
Alex Werner <alex@werner.fr>
2020-03-10 15:23:39 +0100

    fix: releasing on new unused wallet

--- src/plugins/Workers/SyncWorker/initialSyncUp.js
+++ src/plugins/Workers/SyncWorker/initialSyncUp.js
@@ -1,5 +1,6 @@
 const EVENTS = require('../../../EVENTS');
 const logger = require('../../../logger');
+
 module.exports = async function initialSyncUp() {
   const self = this;
   // For each new transaction emitted by transporter, we import to storage
@@ -15,7 +16,8 @@ module.exports = async function initialSyncUp() {
   });
 
   // Due to the events system, we need to handle the fact that we did subscribed to addresses
-  // that we had received the transactions and store before being able to release initialSyncUp as ready.
+  // that we had received the transactions and store before
+  // being able to release initialSyncUp as ready.
   // When we will move to bloomfilter, that part might be more complex.
   return new Promise((resolve) => {
     const addrList = this.getAddressListToSync().map((addr) => addr.address);
@@ -42,6 +44,10 @@ module.exports = async function initialSyncUp() {
       logger.silly(`SyncWorker - init addrWaiter : fetched : ${numberOfFetchedAddr}/${expectedAddrNumberToFetch}`);
 
       if (numberOfFetchedAddr >= expectedAddrNumberToFetch) {
+        if (expectedTxNumberToFetch === 0) {
+          self.transporter.removeListener(EVENTS.FETCHED_TRANSACTION, waiterTxFetchedListernerFn);
+          resolve(true);
+        }
         self.transporter.removeListener(EVENTS.FETCHED_ADDRESS, waiterAddressFetchedListenerFn);
       }
     };
