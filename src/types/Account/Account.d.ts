9b35dd386e88296a0c79292f4978631439474cc2
Alex Werner <alex@werner.fr>
2020-04-15 08:01:52 +0200

    refact: removed DPA Plugin type

--- src/types/Account/Account.d.ts
+++ src/types/Account/Account.d.ts
@@ -26,8 +26,6 @@ export declare class Account {
     getUnconfirmedBalance(displayDuffs?:boolean): number;
     getBIP44Path(network?:Network, index?:number): string;
 
-    getDPA(name: string): object;
-
     getIdentityHDKey(index: number, identityType: string): HDPrivateKey;
 
     getNetwork(): Network;
