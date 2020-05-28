c0f060fb115679eb0f828d73c12d5db533f0543b
Alex Werner <alex@werner.fr>
2020-05-28 13:16:20 +0200

    feat: added network as DAPIClient params

--- src/transporters/types/DAPIClient/DAPIClient.js
+++ src/transporters/types/DAPIClient/DAPIClient.js
@@ -8,6 +8,7 @@ class DAPIClient extends BaseTransporter {
       // This allows to not have dapi-client shipped by default.
       // eslint-disable-next-line global-require,import/no-extraneous-dependencies
       const Client = require('@dashevo/dapi-client');
+      this.network = props.network || 'testnet';
       this.client = new Client(props);
     } catch (err) {
       logger.error("The '@dashevo/dapi-client' package is missing! Please install it with 'npm install @dashevo/dapi-client --save' command.");
@@ -31,6 +32,7 @@ DAPIClient.prototype.getTransaction = require('./methods/getTransaction');
 DAPIClient.prototype.getUTXO = require('./methods/getUTXO');
 DAPIClient.prototype.sendTransaction = require('./methods/sendTransaction');
 DAPIClient.prototype.subscribeToAddressesTransactions = require('./methods/subscribeToAddressesTransactions');
+DAPIClient.prototype.subscribeToTransactionsWithProofs = require('./methods/subscribeToTransactionsWithProofs');
 DAPIClient.prototype.subscribeToBlockHeaders = require('./methods/subscribeToBlockHeaders');
 DAPIClient.prototype.subscribeToBlocks = require('./methods/subscribeToBlocks');
 
