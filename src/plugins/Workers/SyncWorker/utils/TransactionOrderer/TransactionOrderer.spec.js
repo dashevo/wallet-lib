5e5336c813ebd60a4b91b463a66df9f44ac9fa33
Alex Werner <alex@werner.fr>
2020-06-09 00:14:04 +0200

    test: added more complex inputs tests
    + permutate 200 times to tests various combinations

--- src/plugins/Workers/SyncWorker/utils/TransactionOrderer/TransactionOrderer.spec.js
+++ src/plugins/Workers/SyncWorker/utils/TransactionOrderer/TransactionOrderer.spec.js
@@ -1,6 +1,7 @@
 const {Transaction} = require('@dashevo/dashcore-lib');
 const {expect} = require('chai');
 const TransactionOrderer = require('./TransactionOrderer');
+const transactionsMultiFixtures = require('../../../../../../fixtures/plugins/SyncWorker/transactions.set.0.json');
 const transactionsFixtures = require('../../../../../../fixtures/plugins/SyncWorker/transactions.set.1.json');
 
 const tx0 = new Transaction(transactionsFixtures[0]); // Predecessor : none (from faucet); Successor : tx1 & tx20 (dup)
@@ -58,16 +59,16 @@ describe('SyncWorker - utils - TransactionOrderer', () => {
       expect(transactionOrderer.transactions[1]).to.equal(tx1);
     });
   });
-  describe('.lookup', ()=>{
+  describe('.lookup', () => {
     it('should be able to lookup from hash', function () {
       const tx = transactionOrderer.lookupByTransactionHash(tx0.hash);
-      expect(tx).to.deep.equal({tx:tx0, pos:0});
+      expect(tx).to.deep.equal({tx: tx0, pos: 0});
 
       const notFound = transactionOrderer.lookupByTransactionHash(tx41.hash);
       expect(notFound).to.deep.equal(null);
     });
   });
-  describe('.insertion predecessors', ()=> {
+  describe('.insertion predecessors', () => {
     // TODO: multiples inputs tests and ordered verifications.
     it('should lookup inputs predecessors', function () {
       const res1 = transactionOrderer.lookupInputsPredecessors(tx23);
@@ -78,23 +79,23 @@ describe('SyncWorker - utils - TransactionOrderer', () => {
       expect(res2).to.deep.equal([{tx: tx0, pos: 0}]);
     });
   });
-  describe('.reset', ()=>{
+  describe('.reset', () => {
     it('should reset', function () {
       transactionOrderer.reset();
       expect(transactionOrderer.transactions.length).to.equal(0);
       expect(transactionOrderer.transactionIds.length).to.equal(0);
     });
   });
-  describe('.insertion successors', ()=>{
+  describe('.insertion successors', () => {
     // Todo: with more
     it('should lookup txid successors', function () {
       transactionOrderer.transactions = [tx1];
       const res = transactionOrderer.lookupTxIdSuccessors(tx0.hash);
       expect(res.length).to.equal(1);
-      expect(res[0]).to.deep.equal({tx:tx1, pos:0});
+      expect(res[0]).to.deep.equal({tx: tx1, pos: 0});
     });
   });
-  describe('.insertion full-case', ()=>{
+  describe('.insertion full-case', () => {
     it('should insert after a predecessor', function () {
       transactionOrderer.reset();
       transactionOrderer.insert(tx0);
@@ -168,7 +169,7 @@ describe('SyncWorker - utils - TransactionOrderer', () => {
       expect(transactionOrderer.transactions[2].hash).to.equal(tx21.hash);
     });
     it('should extract and replace', function () {
-     transactionOrderer.reset();
+      transactionOrderer.reset();
       transactionOrderer.insert(tx21);
       transactionOrderer.insert(tx0);
       transactionOrderer.insert(tx1);
@@ -194,7 +195,38 @@ describe('SyncWorker - utils - TransactionOrderer', () => {
       expect(transactionOrderer.transactions[5].hash).to.equal(tx23.hash);
       expect(transactionOrderer.transactions[6].hash).to.equal(tx3.hash);
       expect(transactionOrderer.transactions.length).to.equal(7);
-      transactionOrderer.reset();
+    });
+    it('should work on complex case', function () {
+      const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+      const verifyExpected = () => {
+        expect(transactionOrderer.transactions.length).to.equal(13);
+        expect(transactionOrderer.transactionIds.length).to.equal(13);
+        const txIds = transactionOrderer.transactionIds;
+        // Tx5 is payment from UTXO of tx4
+        expect(txIds.indexOf(transactionsMultiFixtures[5].txid)).to.gt(txIds.indexOf(transactionsFixtures[4].txid))
+        // Tx6 is payment from UTXO of tx3
+        expect(txIds.indexOf(transactionsMultiFixtures[6].txid)).to.gt(txIds.indexOf(transactionsFixtures[3].txid))
+        // Tx 7 is made from UTXO of tx2 and tx1
+        expect(txIds.indexOf(transactionsMultiFixtures[7].txid)).to.gt(txIds.indexOf(transactionsFixtures[2].txid))
+        expect(txIds.indexOf(transactionsMultiFixtures[7].txid)).to.gt(txIds.indexOf(transactionsFixtures[1].txid))
+
+        // Used from tx7, tx3 and tx4
+        expect(txIds.indexOf(transactionsMultiFixtures[9].txid)).to.gt(txIds.indexOf(transactionsFixtures[7].txid))
+        expect(txIds.indexOf(transactionsMultiFixtures[9].txid)).to.gt(txIds.indexOf(transactionsFixtures[6].txid))
+        expect(txIds.indexOf(transactionsMultiFixtures[9].txid)).to.gt(txIds.indexOf(transactionsFixtures[5].txid))
+
+        expect(txIds.indexOf(transactionsMultiFixtures[11].txid)).to.gt(txIds.indexOf(transactionsFixtures[9].txid))
+        expect(txIds.indexOf(transactionsMultiFixtures[12].txid)).to.gt(txIds.indexOf(transactionsFixtures[11].txid))
+      };
+      Array(200)
+          .fill(0)
+          .forEach((el, i) => {
+            transactionOrderer.reset();
+            shuffle(transactionsMultiFixtures).forEach((tx) => {
+              transactionOrderer.insert(new Transaction(tx.hex));
+            });
+            verifyExpected();
+          });
     });
   })
 });
