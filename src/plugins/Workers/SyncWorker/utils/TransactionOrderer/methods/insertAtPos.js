68cd6f00e9a0ef50b380927a463698628e5cd206
Alex Werner <alex@werner.fr>
2020-06-09 18:59:17 +0200

    style: readability improvement + error instead of magical adjustment

--- src/plugins/Workers/SyncWorker/utils/TransactionOrderer/methods/insertAtPos.js
+++ src/plugins/Workers/SyncWorker/utils/TransactionOrderer/methods/insertAtPos.js
@@ -2,16 +2,24 @@
  * Add a transaction at a specific position
  * @param {Transaction} transaction
  * @param {number?} pos
+ * @return this
  */
 module.exports = function insertAtPos(transaction, pos) {
   const { transactionIds, transactions } = this;
-  if (pos != null) {
-    // eslint-disable-next-line no-param-reassign
-    if (pos > transactions.length) pos = transactions.length;
-    transactions.splice(pos, 0, transaction);
-    transactionIds.splice(pos, 0, transaction.hash);
-  } else {
+
+  if (pos > transactions.length) {
+    throw new Error('Cannot insert in an out of range position');
+  }
+
+  if (pos === null || pos === undefined) {
     transactions.push(transaction);
     transactionIds.push(transaction.hash);
+
+    return this;
   }
+
+  transactions.splice(pos, 0, transaction);
+  transactionIds.splice(pos, 0, transaction.hash);
+
+  return this;
 };
