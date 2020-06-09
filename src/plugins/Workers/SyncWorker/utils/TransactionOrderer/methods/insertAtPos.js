6f06f05f75fa89816e5273b10a54ca39029135ed
Alex Werner <alex@werner.fr>
2020-06-09 19:41:51 +0200

    doc(insertAtPos): fix JSDoc

--- src/plugins/Workers/SyncWorker/utils/TransactionOrderer/methods/insertAtPos.js
+++ src/plugins/Workers/SyncWorker/utils/TransactionOrderer/methods/insertAtPos.js
@@ -1,17 +1,17 @@
 /**
  * Add a transaction at a specific position
  * @param {Transaction} transaction
- * @param {number?} pos
+ * @param {number} [pos]
  * @return this
  */
-module.exports = function insertAtPos(transaction, pos) {
+module.exports = function insertAtPos(transaction, pos = undefined) {
   const { transactionIds, transactions } = this;
 
   if (pos > transactions.length) {
     throw new Error('Cannot insert in an out of range position');
   }
 
-  if (pos === null || pos === undefined) {
+  if (pos === undefined) {
     transactions.push(transaction);
     transactionIds.push(transaction.hash);
 
