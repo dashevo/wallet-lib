3487ff7e3bb16a250b02f8c4d0b2ee8fd0fdef5e
Alex Werner <alex@werner.fr>
2020-06-15 19:06:52 +0200

    impr(BIP44Worker): return generated value on execute

--- src/plugins/Workers/BIP44Worker/BIP44Worker.js
+++ src/plugins/Workers/BIP44Worker/BIP44Worker.js
@@ -18,7 +18,8 @@ class BIP44Worker extends Worker {
     // Following BIP44 Account Discovery section, we will scan the external chain of this account.
     // We do not need to scan the internal as it's linked to external's one
     // So we just seek for 1:1 internal of external.
-    this.ensureEnoughAddress();
+    const generated = this.ensureEnoughAddress();
+    return {generated};
   }
 }
 
