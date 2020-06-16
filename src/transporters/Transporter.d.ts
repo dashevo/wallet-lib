9b722ef2226eb978e261ef1aec1ab87cfc30ba3d
Alex Werner <alex@werner.fr>
2020-06-16 23:35:59 +0200

    doc(Transporter): definition for functions

--- src/transporters/Transporter.d.ts
+++ src/transporters/Transporter.d.ts
@@ -1,3 +1,8 @@
+import {Block} from "@dashevo/dashcore-lib/typings/block/Block";
+import {BlockHeader} from "@dashevo/dashcore-lib/typings/block/BlockHeader";
+import {Network} from "../types/types";
+import {Transaction} from "@dashevo/dashcore-lib/typings/transaction/Transaction";
+
 export declare class BaseTransporter {
     constructor(props: BaseTransporterOptions);
     type: string;
@@ -14,6 +19,52 @@ export declare class BaseTransporter {
             addresses: any
         }
     }
+
+    announce(eventName: string, args: any): void;
+    disconnect(): boolean;
+    getAddressSummary(address: string): any;
+    getBestBlock(): Promise<Block>;
+    getBestBlockHeader(): Promise<BlockHeader>;
+    getBestBlockHash(): Promise<string>;
+    getBestBlockHeight(): Promise<number>;
+    getBlockByHash(hash: string): Promise<Block>;
+    getBlockByHeight(height: number): Promise<Block>;
+    getBlockHeaderByHash(hash: string): Promise<BlockHeader>;
+    getBlockHeaderByHeight(height: number): Promise<BlockHeader>;
+    getStatus(height: number): Promise<{
+        coreVersion: number,
+        protocolVersion: number,
+        blocks: number,
+        timeOffset: number,
+        connections: number,
+        proxy: string,
+        difficulty: number,
+        testnet: false,
+        relayFee: number,
+        errors: string,
+        network: Network,
+    }>;
+    getTransaction(hash: number): Promise<Transaction>;
+    getUTXO(address: string):Promise<{
+        totalItems: number,
+        from: number,
+        to: number,
+        items: [
+            {
+                address: string,
+                txid: string,
+                outputIndex: number,
+                script: string,
+                satoshis: number,
+                height: number,
+            },
+        ],
+    }>;
+    sendTransaction(rawtx: string): Promise<string>
+    subscribeToAddressesTransactions(addressList:[string]): Promise<void>
+    subscribeToBlockHeaders(): Promise<void>
+    subscribeToBlocks(): Promise<void>
+    getIdentityIdByFirstPublicKey(publicKeyHash: string): Promise<string|null>
 }
 
 export declare interface BaseTransporterOptions {
