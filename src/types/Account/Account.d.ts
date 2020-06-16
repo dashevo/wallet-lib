d144a3e47959e28bef6d8e190eccec8d568c5a2d
Alex Werner <alex@werner.fr>
2020-06-16 19:19:58 +0200

    doc(Account.d.ts): update account default parameters

--- src/types/Account/Account.d.ts
+++ src/types/Account/Account.d.ts
@@ -1,14 +1,31 @@
-import {Mnemonic, Transaction, AddressObj,AddressInfo, AddressType, transactionId, TransactionInfo, PublicAddress, PrivateKey, Strategy, Network, Plugins} from "../types";
-import {KeyChain} from "../KeyChain/KeyChain";
-import {HDPrivateKey} from "@dashevo/dashcore-lib";
-import {Wallet} from "../../index";
+import {
+    Mnemonic,
+    Transaction,
+    AddressObj,
+    AddressInfo,
+    AddressType,
+    transactionId,
+    TransactionInfo,
+    PublicAddress,
+    PrivateKey,
+    Strategy,
+    Network,
+    Plugins
+} from "../types";
+import { KeyChain } from "../KeyChain/KeyChain";
+import { HDPrivateKey } from "@dashevo/dashcore-lib";
+import { Wallet } from "../../index";
 
 export declare class Account {
     constructor(wallet: Wallet, options?: Account.Options);
+
     index: number;
-    debug?: boolean;
+    injectDefaultPlugins?: boolean = true;
+    allowSensitiveOperations?: boolean = false;
+    debug?: boolean = false;
+    strategy?: Strategy = simpleTransactionOptimizedAccumulator;
     keyChain: KeyChain;
-    state:any;
+    state: any;
 
     isReady(): Promise<boolean>;
     isInitialized(): Promise<boolean>;
@@ -16,58 +33,57 @@ export declare class Account {
     connect(): boolean;
     createTransaction(opts: Account.createTransactionOptions): Transaction;
     disconnect(): boolean;
-    fetchAddressInfo(addresObj: AddressObj, fetchUtxo?:boolean): Promise<AddressInfo|false>;
-    fetchStatus(): Promise<object|false>
+    fetchAddressInfo(addresObj: AddressObj, fetchUtxo?: boolean): Promise<AddressInfo | false>;
+    fetchStatus(): Promise<object | false>
     forceRefreshAccount(): boolean;
-    generateAddress(path:string): AddressObj;
-    getAddress(index:number, _type: AddressType): AddressObj;
+    generateAddress(path: string): AddressObj;
+    getAddress(index: number, _type: AddressType): AddressObj;
     getAddresses(rawtx: string, isIS: boolean): [AddressObj];
-    getTotalBalance(displayDuffs?:boolean): number;
-    getConfirmedBalance(displayDuffs?:boolean): number;
-    getUnconfirmedBalance(displayDuffs?:boolean): number;
-    getBIP44Path(network?:Network, index?:number): string;
-
+    getTotalBalance(displayDuffs?: boolean): number;
+    getConfirmedBalance(displayDuffs?: boolean): number;
+    getUnconfirmedBalance(displayDuffs?: boolean): number;
+    getBIP44Path(network?: Network, index?: number): string;
     getNetwork(): Network;
-
-    getPlugin(name:string): object;
-
-    getPrivateKeys(addressList:[PublicAddress]): [PrivateKey];
+    getPlugin(name: string): object;
+    getPrivateKeys(addressList: [PublicAddress]): [PrivateKey];
     getTransaction(txid: transactionId): Transaction;
     getTransactionHistory(): [object];
     getTransactions(): [Transaction];
     getUnusedAddress(type?: AddressType, skip?: number): AddressObj;
     getUTXOS(): [object];
-    injectPlugin(unsafePlugin: Plugins, allowSensitiveOperation:boolean): Promise<boolean>;
-    sign(object?:Transaction, privateKeys?:[PrivateKey], sigType?:string): Transaction;
+    injectPlugin(unsafePlugin: Plugins, allowSensitiveOperation: boolean): Promise<boolean>;
+    sign(object?: Transaction, privateKeys?: [PrivateKey], sigType?: string): Transaction;
     updateNetwork(network: Network): boolean;
-
     getIdentityIds(): string[];
     getIdentityHDKeyById(identityId: string, keyIndex: number): HDPrivateKey;
     getIdentityHDKeyByIndex(identityIndex: number, keyIndex: number): HDPrivateKey;
     getUnusedIdentityIndex(): Promise<number>;
 }
+
 export declare interface RecipientOptions {
     satoshis?: number;
     amount?: number;
     address: PublicAddress,
 }
+
 export declare namespace Account {
     interface Options {
-        index?:number,
+        index?: number,
         network?: Network;
         debug?: boolean;
-        allowSensitiveOperations?: string;
         plugins?: [Plugins];
         cacheBlockHeaders?: boolean;
         cacheTx?: boolean;
-        injectDefaultPlugins?: string;
+        allowSensitiveOperations?: boolean;
+        injectDefaultPlugins?: boolean;
         strategy?: Strategy;
     }
+
     interface createTransactionOptions {
-        recipient?:RecipientOptions,
-        recipients?:[RecipientOptions]
+        recipient?: RecipientOptions,
+        recipients?: [RecipientOptions]
         change?: string;
-        utxos?:[object];
+        utxos?: [object];
         isInstantSend?: boolean;
         deductFee?: boolean
         privateKeys?: [PrivateKey],
