ed0a8955c819822b40274ce83fd13a6c879f1743
Alex Werner <alex@werner.fr>
2020-06-17 12:00:52 +0200

    doc(KeyChain): update definitions

--- src/types/KeyChain/KeyChain.d.ts
+++ src/types/KeyChain/KeyChain.d.ts
@@ -2,46 +2,52 @@ import {PrivateKey, Network,} from "../types";
 import {HDPrivateKey, HDPublicKey} from "@dashevo/dashcore-lib";
 import {Transaction} from "@dashevo/dashcore-lib/typings/transaction/Transaction";
 
+export declare namespace KeyChain {
+    const defaultKeyChainOptions: KeyChain.IKeyChainOptions = {
+        network: 'testnet',
+        keys: {}
+    };
+    interface IKeyChainOptions {
+        network?: Network;
+        keys?: [Keys]
+    }
+}
+
 export declare class KeyChain {
-    constructor(options?: KeyChain.Options);
+    constructor(options?: KeyChain.IKeyChainOptions = KeyChain.defaultKeyChainOptions);
     network: Network;
     keys: [Keys];
 
-    // todo valid keychain type object.
-    type: string;
+    type: HDKeyTypesParam|PrivateKeyTypeParam;
     HDPrivateKey?: HDPrivateKey;
     privateKey?: PrivateKey;
 
-
     generateKeyForChild(index: number, type?: HDKeyTypesParam): HDPrivateKey|HDPublicKey;
-    generateKeyForPath(path: string, type?: HDKeyTypesParam): HDKeyTypes;
+    generateKeyForPath(path: string, type?: HDKeyTypesParam): HDPrivateKey|HDPublicKey;
 
     getHardenedBIP44Path(type?: HDKeyTypesParam): HDKeyTypes;
     getHardenedDIP9FeaturePath(type?: HDKeyTypesParam): HDKeyTypes;
 
     getKeyForChild(index: number, type?: HDKeyTypesParam): HDKeyTypes;
     getKeyForPath(path: string, type?: HDKeyTypesParam): HDKeyTypes;
-    getPrivateKey(): HDPrivateKey|PrivateKey;
+    getPrivateKey(): PrivateKey;
 
-    sign(object: Transaction, privateKeys:[any], sigType: number): any;
-
+    sign(object: Transaction|any, privateKeys:[PrivateKey], sigType: number): any;
 }
+
 type HDKeyTypes = HDPublicKey | HDPrivateKey;
 
 export declare enum HDKeyTypesParam {
     HDPrivateKey="HDPrivateKey",
     HDPublicKey="HDPrivateKey",
 }
+export declare enum PrivateKeyTypeParam {
+    privateKey='privateKey'
+}
 export declare interface Keys {
     [path: string]: {
         path: string
     };
-}
-export declare namespace KeyChain {
-    interface Options {
-        network?: Network;
-        keys?: [Keys]
-    }
 }
 
 
