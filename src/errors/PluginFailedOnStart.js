42c6654e3f36133fad5c1e1707bf0c1a582ec07f
Alex Werner <alex@werner.fr>
2020-08-03 10:19:51 +0200

    impr(PluginFailedOnStart): capture stack trace on failure

--- src/errors/PluginFailedOnStart.js
+++ src/errors/PluginFailedOnStart.js
@@ -1,8 +1,9 @@
 const WalletLibError = require('./WalletLibError');
 
 class PluginFailedOnStart extends WalletLibError {
-  constructor(pluginType, pluginName, reason = 'unknown') {
+  constructor(pluginType, pluginName, reason = 'unknown', err) {
     super(`Plugin ${pluginName} of type ${pluginType} onStart failed. Reason: ${reason}`);
+    if (err.stack) this.stack = err.stack;
   }
 }
 
