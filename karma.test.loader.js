893525c5bfdec4d371b80c73e74eacf6a9830552
Anton Suprunchuk <antouhou@gmail.com>
2020-08-17 18:37:23 +0300

    Fix browser tests

--- karma.test.loader.js
+++
@@ -1,5 +0,0 @@
-const testsContext = require.context('./src', true, /spec.js$/);
-const integrationTestsContext = require.context('./tests/integration', true, /spec.js$/);
-
-testsContext.keys().forEach(testsContext);
-integrationTestsContext.keys().forEach(integrationTestsContext);
