863d4225b202ac0d793cbc43b7182f0fb5d3f836
Alex Werner <alex@werner.fr>
2020-09-01 03:33:41 +0200

    feat: ensure maturity of coinbase output

--- src/types/Account/methods/getUTXOS.js
+++ src/types/Account/methods/getUTXOS.js
@@ -1,5 +1,6 @@
 const { Address, Transaction } = require('@dashevo/dashcore-lib');
-
+const { NULL_HASH } = require('../../../CONSTANTS');
+const NULL_BUFFER = Buffer.from(NULL_HASH, 'hex');
 /**
  * Return all the utxos
  * @return {UnspentOutput[]}
@@ -9,6 +10,7 @@ function getUTXOS() {
 
   const self = this;
   const { walletId, network } = this;
+  const currentBlockHeight = this.store.chains[network].blockHeight;
   /* eslint-disable-next-line no-restricted-syntax */
   for (const walletType in this.store.wallets[walletId].addresses) {
     if (walletType && ['external', 'internal', 'misc'].includes(walletType)) {
@@ -20,7 +22,17 @@ function getUTXOS() {
           for (const identifier in address.utxos) {
             if (identifier) {
               const [txid, outputIndex] = identifier.split('-');
-
+              const transaction = this.store.transactions[txid];
+              const isCoinbase = Buffer.compare(transaction.inputs[0].prevTxId, NULL_BUFFER) === 0;
+              if (isCoinbase) {
+                // We check maturity is at least 100 blocks.
+                const { height } = transaction.extraPayload;
+                // another way is to just read _scriptBuffer height value.
+                if (height + 100 > currentBlockHeight) {
+                  // eslint-disable-next-line no-continue
+                  continue;
+                }
+              }
               utxos.push(new Transaction.UnspentOutput(
                 {
                   txId: txid,
