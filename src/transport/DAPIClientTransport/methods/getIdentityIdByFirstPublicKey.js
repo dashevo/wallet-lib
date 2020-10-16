823afd4c1622de852f953b8518cddf776346a129
Anton Suprunchuk <antouhou@gmail.com>
2020-10-16 14:04:51 +0300

    return null if identity id is null

--- src/transport/DAPIClientTransport/methods/getIdentityIdByFirstPublicKey.js
+++ src/transport/DAPIClientTransport/methods/getIdentityIdByFirstPublicKey.js
@@ -8,5 +8,5 @@ module.exports = async function getIdentityIdByFirstPublicKey(publicKeyHash) {
     [publicKeyHash],
   );
 
-  return new Identifier(identityId).toString();
+  return identityId || new Identifier(identityId).toString();
 };
