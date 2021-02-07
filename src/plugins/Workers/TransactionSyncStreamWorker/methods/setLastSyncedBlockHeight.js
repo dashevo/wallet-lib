13577657a6b54cecbdf9e97c139e262cf24dd6bb
Alex Werner <alex@werner.fr>
2021-02-07 21:38:47 +0100

    feat: setLastSyncedBlockHeight to provide hash in accountStore

--- src/plugins/Workers/TransactionSyncStreamWorker/methods/setLastSyncedBlockHeight.js
+++ src/plugins/Workers/TransactionSyncStreamWorker/methods/setLastSyncedBlockHeight.js
@@ -7,14 +7,18 @@ const { WALLET_TYPES } = require('../../../../CONSTANTS');
  * @return {number}
  */
 module.exports = function setLastSyncedBlockHeight(blockHeight) {
-  const { walletId } = this;
+  const { walletId, transport } = this;
   const accountsStore = this.storage.store.wallets[walletId].accounts;
 
   const accountStore = (this.walletType === WALLET_TYPES.SINGLE_ADDRESS)
-    ? accountsStore[this.index.toString()]
-    : accountsStore[this.BIP44PATH.toString()];
+      ? accountsStore[this.index.toString()]
+      : accountsStore[this.BIP44PATH.toString()];
 
   accountStore.blockHeight = blockHeight;
+
+  transport.client.core.getBlockHash(blockHeight).then((hash) => {
+    accountStore.blockHash = hash;
+  });
 
   return accountStore.blockHeight;
 };
