edf48baed05fdf0f6b8a717ce5b75a923fe965a4
Alex Werner <alex@werner.fr>
2021-04-19 14:44:15 +0200

    feat: created waitForBlocks

---
+++ src/utils/waitForBlocks.js
@@ -0,0 +1,23 @@
+const EVENTS = require('../EVENTS');
+/**
+ *
+ * @param {number} blockNumberToWait
+ * @return {Promise<boolean>}
+ */
+module.exports = async function waitForBlocks(account, blockNumberToWait= 1) {
+  let waitedBlock = 0;
+  // Subscribe to block with a faster rate
+  await account.transport.subscribeToBlocks(10*1000);
+
+  return new Promise((resolve) => {
+    const listener = (event) => {
+      waitedBlock += 1;
+      if(waitedBlock>=blockNumberToWait){
+        account.removeListener(EVENTS.BLOCK, listener);
+        resolve(true);
+      }
+    };
+
+    account.on(EVENTS.BLOCK, listener);
+  });
+}
