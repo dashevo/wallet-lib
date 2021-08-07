62febf1d2c03266f865f473baff391e9a1e4bef5
Alex Werner <alex@werner.fr>
2021-08-08 00:34:56 +0200

    feat(Account)!: getTransaction returns metadata when able

--- src/types/Account/methods/getTransaction.js
+++ src/types/Account/methods/getTransaction.js
@@ -1,25 +1,40 @@
 /**
  * Get a transaction from a provided txid
  * @param {transactionId} txid - Transaction Hash
- * @return {Promise<Transaction>}
+ * @return {Promise<{metadata: {blockHash, chainLocked, instantLocked, height}, transaction}>}
  */
 async function getTransaction(txid = null) {
   const search = await this.storage.searchTransaction(txid);
   if (search.found) {
     return search.result;
   }
-  const tx = await this.transport.getTransaction(txid);
-  if (this.cacheTx && tx) {
-    await this.importTransactions(tx);
+  const getTransactionResponse = await this.transport.getTransaction(txid);
+  if (!getTransactionResponse) return null;
+  const {
+    transaction,
+    blockHash,
+    height,
+    instantLocked,
+    chainLocked,
+  } = getTransactionResponse;
+
+  const metadata = {
+    blockHash,
+    height,
+    instantLocked,
+    chainLocked,
+  };
+  if (this.cacheTx) {
+    await this.importTransactions([transaction, metadata]);
     if (this.cacheBlockHeaders) {
-      const searchBlockHeader = this.storage.searchBlockHeader(tx.nLockTime);
+      const searchBlockHeader = this.storage.searchBlockHeader(height);
       if (!searchBlockHeader.found) {
         // Trigger caching of blockheader
-        await this.getBlockHeader(tx.nLockTime);
+        await this.getBlockHeader(height);
       }
     }
   }
-  return tx;
+  return { transaction, metadata };
 }
 
 module.exports = getTransaction;
