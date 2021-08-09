e7d5126e84dbf8ec8dd324d06ee6d3092622bb01
Alex Werner <alex@werner.fr>
2021-08-09 23:25:51 +0200

    feat: move utils to single file
    Will help for testing and managing properly

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,28 +1,32 @@
+const extendTransactionsWithMetadata = require('./extendTransactionsWithMetadata');
+const categorizeTransactions = require('./categorizeTransactions');
+const filterTransactions = require('./filterTransactions');
+const { hash, doubleSha256, sha256 } = require('./crypto');
 const { varIntSizeBytesFromLength } = require('./varInt');
-const {
-  dashToDuffs,
-  duffsToDash,
-  getBytesOf,
-  hasProp,
-  hasMethod,
-} = require('./utils');
+const feeCalculation = require('./feeCalculation');
+const coinSelection = require('./coinSelection');
+const fundWallet = require('./fundWallet');
+const dashToDuffs = require('./dashToDuffs');
+const duffsToDash = require('./duffsToDash');
+const getBytesOf = require('./getBytesOf');
+const hasMethod = require('./hasMethod');
+const hasProp = require('./hasProp');
+const mnemonic = require('./mnemonic');
+const is = require('./is');
+
 const {
   generateNewMnemonic,
   mnemonicToHDPrivateKey,
   mnemonicToWalletId,
   seedToHDPrivateKey,
   mnemonicToSeed,
-} = require('./mnemonic');
-const is = require('./is');
-const coinSelection = require('./coinSelection');
-const feeCalculation = require('./feeCalculation');
-const { hash, doubleSha256, sha256 } = require('./crypto');
-
-const fundWallet = require('./fundWallet');
+} = mnemonic;
 
 module.exports = {
+  categorizeTransactions,
   dashToDuffs,
   duffsToDash,
+  extendTransactionsWithMetadata,
   generateNewMnemonic,
   mnemonicToHDPrivateKey,
   mnemonicToWalletId,
@@ -31,6 +35,7 @@ module.exports = {
   is,
   coinSelection,
   feeCalculation,
+  filterTransactions,
   varIntSizeBytesFromLength,
   getBytesOf,
   hash,
