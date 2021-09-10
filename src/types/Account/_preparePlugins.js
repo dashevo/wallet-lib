1a8853bfb1806d9b1d71e0594f4588986d6fada8
Alex Werner <alex@werner.fr>
2021-09-10 12:17:20 +0200

    feat: optimisation of sequential insert

--- src/types/Account/_preparePlugins.js
+++ src/types/Account/_preparePlugins.js
@@ -1,15 +1,17 @@
 const sortPlugins = require('./_sortPlugins');
 
 const preparePlugins = function preparePlugins(account, userUnsafePlugins) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
+  function reducer(accumulatorPromise, [plugin, allowSensitiveOperation, awaitOnInjection]) {
+    return accumulatorPromise
+      .then(() => account.injectPlugin(plugin, allowSensitiveOperation, awaitOnInjection));
+  }
+
+  return new Promise((resolve, reject) => {
     try {
       const sortedPlugins = sortPlugins(account, userUnsafePlugins);
-      // eslint-disable-next-line no-restricted-syntax
-      for (const [plugin, allowSensitiveOperation, awaitOnInjection] of sortedPlugins) {
-        // eslint-disable-next-line no-await-in-loop
-        await account.injectPlugin(plugin, allowSensitiveOperation, awaitOnInjection);
-      }
+      // It is important that all plugin got successfully injected in a sequential maneer
+      sortedPlugins.reduce(reducer, Promise.resolve()).then(() => resolve(sortedPlugins));
+
       resolve(sortedPlugins);
     } catch (e) {
       reject(e);
