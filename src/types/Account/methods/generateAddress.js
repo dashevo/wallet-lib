2c55741cea0621966501cb4c6aa980b5d08b18bf
Alex Werner <alex@werner.fr>
2021-11-02 02:47:00 +0100

    feat: add generated address to watched address

--- src/types/Account/methods/generateAddress.js
+++ src/types/Account/methods/generateAddress.js
@@ -1,12 +1,14 @@
 const EVENTS = require('../../../EVENTS');
 const { WALLET_TYPES } = require('../../../CONSTANTS');
 const { is } = require('../../../utils');
+
 /**
  * Generate an address from a path and import it to the store
  * @param {string} path
+ * @param {boolean} [isWatchedAddress=true] - if the address will be watched
  * @return {AddressObj} Address information
  * */
-function generateAddress(path) {
+function generateAddress(path, isWatchedAddress = true) {
   if (is.undefOrNull(path)) throw new Error('Expected path to generate an address');
   let index = 0;
   let privateKey;
@@ -19,26 +21,42 @@ function generateAddress(path) {
       break;
     case WALLET_TYPES.PUBLICKEY:
       address = this.keyChainStore.getWalletKeyChain().rootKey.toAddress(network).toString();
+      if (isWatchedAddress) {
+        this.keyChainStore.getWalletKeyChain()
+          .addKeysToWatchedKeys(this.keyChainStore.getWalletKeyChain().rootKey);
+      }
       break;
     case WALLET_TYPES.HDWALLET:
       // eslint-disable-next-line prefer-destructuring
       index = parseInt(path.toString().split('/')[5], 10);
       privateKey = this.keyChainStore.getWalletKeyChain().getKeyForPath(path);
+      if (isWatchedAddress) {
+        this.keyChainStore.getWalletKeyChain().addKeysToWatchedKeys(privateKey);
+      }
       address = privateKey.publicKey.toAddress(network).toString();
       break;
     case WALLET_TYPES.HDPUBLIC:
       index = parseInt(path.toString().split('/')[5], 10);
       privateKey = this.keyChainStore.getWalletKeyChain().getKeyForPath(index);
+      if (isWatchedAddress) {
+        this.keyChainStore.getWalletKeyChain().addKeysToWatchedKeys(privateKey);
+      }
       address = privateKey.publicKey.toAddress(network).toString();
       break;
-    // TODO: DEPRECATE USAGE OF SINGLE_ADDRESS in favor or PRIVATEKEY
+      // TODO: DEPRECATE USAGE OF SINGLE_ADDRESS in favor or PRIVATEKEY
     case WALLET_TYPES.PRIVATEKEY:
     case WALLET_TYPES.SINGLE_ADDRESS:
       privateKey = this.keyChainStore.getWalletKeyChain().rootKey;
+      if (isWatchedAddress) {
+        this.keyChainStore.getWalletKeyChain().addKeysToWatchedKeys(privateKey);
+      }
       address = privateKey.publicKey.toAddress(network).toString();
       break;
     default:
       privateKey = this.keyChainStore.getWalletKeyChain().getKeyForPath(path.toString());
+      if (isWatchedAddress) {
+        this.keyChainStore.getWalletKeyChain().addKeysToWatchedKeys(privateKey);
+      }
       address = privateKey.publicKey.toAddress(network).toString();
   }
 
@@ -59,4 +77,5 @@ function generateAddress(path) {
   this.emit(EVENTS.GENERATED_ADDRESS, { type: EVENTS.GENERATED_ADDRESS, payload: addressData });
   return addressData;
 }
+
 module.exports = generateAddress;
