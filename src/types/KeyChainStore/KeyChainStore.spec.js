12a94f6b8d87d9f945f605a410bde9c339bb30d2
Alex Werner <obusco@gmail.com>
2021-11-05 10:29:33 +0100

    tests(KeyChainStore): update tests

--- src/types/KeyChainStore/KeyChainStore.spec.js
+++ src/types/KeyChainStore/KeyChainStore.spec.js
@@ -6,7 +6,10 @@ const { expect } = require('chai');
 describe('KeyChainStore', function suite() {
   let keyChainsStore;
   let hdPrivateKey = new HDPrivateKey()
+  let hdPublicKey = new HDPrivateKey().hdPublicKey
   let keyChain = new KeyChain(hdPrivateKey)
+  let keyChainPublic = new KeyChain(hdPublicKey)
+  let walletKeyChain = new KeyChain(new HDPrivateKey());
   it('should create a KeyChainStore', () => {
     keyChainsStore = new KeyChainsStore();
     expect(keyChainsStore).to.exist;
@@ -15,10 +18,24 @@ describe('KeyChainStore', function suite() {
   it('should be able to add a keyChain', function () {
     keyChainsStore.addKeyChain(keyChain)
     expect(keyChainsStore.chains.has(keyChain.keyChainId)).to.equal(true);
+    keyChainsStore.addKeyChain(keyChainPublic)
+    expect(keyChainsStore.chains.has(keyChainPublic.keyChainId)).to.equal(true);
+  });
+  it('should allow to specify a specific wallet keychain', function () {
+    keyChainsStore.addKeyChain(walletKeyChain, { isWalletKeyChain: true });
+    expect(keyChainsStore.chains.has(walletKeyChain.keyChainId)).to.equal(true);
   });
   it('should get all keyChains', function () {
     const keyChains = keyChainsStore.getKeyChains()
-    expect(keyChains).to.deep.equal([keyChain]);
+    expect(keyChains).to.deep.equal([keyChain, keyChainPublic, walletKeyChain]);
+  });
+  it('should get a keychain by its ID', () => {
+    const requestedKeychain = keyChainsStore.getKeyChain(keyChainPublic.keyChainId);
+    expect(requestedKeychain).to.equal(keyChainPublic);
+  })
+  it('should get a wallet keychain', function () {
+    const requestedWalletKeyChain = keyChainsStore.getWalletKeyChain();
+    expect(requestedWalletKeyChain).to.equal(walletKeyChain);
   });
 });
 
