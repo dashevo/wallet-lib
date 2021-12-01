1ba47cc58b0c857540b3d29a545872d8872464b6
markin.io <igor.markin@dash.org>
2021-12-01 12:19:49 +0300

    Fix getTotalBalance.spec.js

--- src/types/Account/methods/getTotalBalance.spec.js
+++ src/types/Account/methods/getTotalBalance.spec.js
@@ -1,43 +1,46 @@
 const { expect } = require('chai');
-const mockedStore = require('../../../../fixtures/sirentonight-fullstore-snapshot-1562711703');
+const walletStoreMock = require('../../../../fixtures/wallets/c922713eac.json');
+const chainStoreMock = require('../../../../fixtures/chains/for_wallet_c922713eac.json');
 const getTotalBalance = require('./getTotalBalance');
 const getConfirmedBalance = require('./getConfirmedBalance');
 const getUnconfirmedBalance = require('./getUnconfirmedBalance');
-const { duffsToDash, calculateDuffBalance } = require('../../../utils');
+const Storage = require("../../Storage/Storage");
 
 
 let mockedWallet;
 describe('Account - getTotalBalance', function suite() {
   this.timeout(10000);
   before(() => {
-    const storageHDW = {
-      store: mockedStore,
-      calculateDuffBalance,
-      getStore: () => mockedStore,
-      mappedAddress: {},
-    };
-    const walletId = Object.keys(mockedStore.wallets)[0];
+    const { walletId } = walletStoreMock;
+
     mockedWallet = {
       walletId,
       index: 0,
-      storage: storageHDW,
+      storage: new Storage(),
+      accountPath: "m/44'/1'/0'",
+      network: "testnet"
     };
-  });
-  it('should correctly get the balance', async () => {
-    const balance = await getTotalBalance.call(mockedWallet);
-    expect(balance).to.equal(184499999506);
-  });
-  it('should correctly get the balance confirmed only', async () => {
-    const balance = await getConfirmedBalance.call(mockedWallet);
-    expect(balance).to.equal(184499999506);
-  });
-  it('should correctly get the balance dash value instead of duff', async () => {
-    const balanceTotalDash = await getTotalBalance.call(mockedWallet, false);
-    const balanceUnconfDash = await getUnconfirmedBalance.call(mockedWallet, false);
-    const balanceConfDash = await getConfirmedBalance.call(mockedWallet, false);
 
-    expect(balanceTotalDash).to.equal(1844.99999506);
+    mockedWallet.storage.createWalletStore(walletId)
+    mockedWallet.storage.createChainStore("testnet")
+    mockedWallet.storage.getWalletStore(walletId).importState(walletStoreMock)
+    mockedWallet.storage.getChainStore("testnet").importState(chainStoreMock)
+  });
+  it('should correctly get the balance',  () => {
+    const balance = getTotalBalance.call(mockedWallet);
+    expect(balance).to.equal(224108673);
+  });
+  it('should correctly get the balance confirmed only',  () => {
+    const balance = getConfirmedBalance.call(mockedWallet);
+    expect(balance).to.equal(224108673);
+  });
+  it('should correctly get the balance dash value instead of duff',  () => {
+    const balanceTotalDash = getTotalBalance.call(mockedWallet, false);
+    const balanceUnconfDash = getUnconfirmedBalance.call(mockedWallet, false);
+    const balanceConfDash = getConfirmedBalance.call(mockedWallet, false);
+
+    expect(balanceTotalDash).to.equal(2.24108673);
     expect(balanceUnconfDash).to.equal(0);
-    expect(balanceConfDash).to.equal(1844.99999506);
+    expect(balanceConfDash).to.equal(2.24108673);
   });
 });
